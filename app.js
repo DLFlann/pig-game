5f0eaf472ba1702070977315b6afff9ac734474b
Dennis Flannigan <DLFlann@gmail.com>
2020-04-14 19:18:03 -0500

    Add basic dice roll functionality

--- app.js
+++ app.js
@@ -8,3 +8,24 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textcontent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textcontent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'img/dice-' + dice + '.png';
+});
